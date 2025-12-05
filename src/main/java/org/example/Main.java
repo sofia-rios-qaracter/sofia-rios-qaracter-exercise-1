e308be96cb0097bb15e249d9a459af9d214b1f25
Sofia <sofia.rios@qaracter.com>
2025-12-05 11:31:20 +0000

    Added some changes on main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,14 @@ public class Main {
         return a+b+c;
     }
 
+    public static int substraction(int a, int b){
+        return a-b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I'm Sofia Rios!!");
         System.out.println("La suma de 2, 3, y 4 es: "+suma(2,3,4));
+        System.out.println("La resta de 5 menos 3 es: "+substraction(5,3));
     }
 }
