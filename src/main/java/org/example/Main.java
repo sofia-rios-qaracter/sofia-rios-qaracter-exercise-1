bf8b9240b82399f86b951c5969786a07bfc4317b
Sofia <sofia.rios@qaracter.com>
2025-12-05 11:25:18 +0000

    Added some changes on main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
 public class Main {
+
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I'm Sofia Rios!!");
+        System.out.println("La suma de 2, 3, y 4 es: "+suma(2,3,4));
     }
 }
